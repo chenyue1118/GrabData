26fcdc0ce689e57e5917f4e3fe773d49359a593f
chenyue <chenyue@yunzongnet.com>
2020-07-21 18:18:36 +0800

    update

--- test/down.js
+++ test/down.js
@@ -7,10 +7,15 @@ const fileUrl = 'https://www.bitahub.com/static/img/icon-fragment1-job.7af6c029.
 const fileName = fileUrl.split('/').pop()
 // const saveFile = `${__dirname}/downFile/${fileName}`
 const saveFile = path.join(__dirname, `/downFile/${fileName}`)
-console.log(saveFile);
+// 测试路径错误时候的ERR
+// const saveFile = path.resolve(`/downFile/${fileName}`)
+console.log('当前存储文件路径', saveFile);
 request(fileUrl)
   // .pipe(fs.createWriteStream(__dirname + '/downFile/doodle.png'))
   .pipe(fs.createWriteStream(saveFile))
   .on('finish', () => {
-    console.log(11111111111111111111);
+    console.log('status', '下载成功');
+  })
+  .on('error', (err) => {
+    console.log('err', err);
   })
