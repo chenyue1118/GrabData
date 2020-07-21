411f9f67b399a884a6ebfa1aa3896f25211aba1c
chenyue <chenyue@yunzongnet.com>
2020-07-21 11:09:57 +0800

    add test

---
+++ test/index.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const app = express()
+
+const superagent = require('superagent')
+require('superagent-proxy')(superagent)
+
+const proxy = 'http://127.0.0.1:53866'
+
+app.use('/', (req, res) => {
+  const url = 'https://www.google.com/'
+  superagent
+    .get(url)
+    .proxy(proxy)
+    .end((err, response) => {
+      if (err) {
+        console.log('err', err)
+        res.send({ 'state': false, message: err })
+      } else {
+        console.log('response', response)
+        res.send({ 'state': true, message: response })
+      }
+    })
+})
+
+app.listen(6003, () => {
+  console.log('Test Server at 6003')
+})
