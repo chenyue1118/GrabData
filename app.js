26126658418d86a6195c1fbe0d41dad49ff93210
chenyue <chenyue@yunzongnet.com>
2020-07-23 17:37:33 +0800

    update

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const express =require('express')
-
 const app = express()
 
-app.use(express.static('./public'))
+const mulrer = ({ dst: '/'})
+app.use(express.static('./upload'))
+
+app.use('/userInfo', (req, res) => {
+  console.log(req,file);
+  send(1)
+})
 
 app.listen(6001, () => {
   console.log('Serve at 6001');
