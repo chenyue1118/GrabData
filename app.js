c9197a028dd8032fcb15e2eea411033cd4e6a537
chenyue <chenyue@yunzongnet.com>
2020-07-31 15:41:32 +0800

    update

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const express =require('express')
 const app = express()
 
-const mulrer = ({ dst: '/'})
+// const mulrer = ({ dst: '/'})
 app.use(express.static('./upload'))
 
-app.use('/userInfo', single(file), (req, res) => {
+app.use('/userInfo', (req, res) => {
   console.log(req.query.file)
   console.log(req,file)
   send(1)
